1e3298cb59d9794ec2bb5894e37cf04bd26bcc0b
Artem <Artemasdwasdw@mail.ru>
2021-05-30 17:41:08 +0300

    start project

---
+++ backend/target/rls/debug/deps/backend-eaf609ad37717a70.d
@@ -0,0 +1,5 @@
+c:\Users\user\Desktop\Cmder\dev\docx-template-generation\backend\target\rls\debug\deps\backend-eaf609ad37717a70.rmeta: src\main.rs
+
+c:\Users\user\Desktop\Cmder\dev\docx-template-generation\backend\target\rls\debug\deps\backend-eaf609ad37717a70.d: src\main.rs
+
+src\main.rs:
---
+++ backend/target/rls/debug/deps/backend-f425bb5e883cf3a5.d
@@ -0,0 +1,5 @@
+c:\Users\user\Desktop\Cmder\dev\docx-template-generation\backend\target\rls\debug\deps\backend-f425bb5e883cf3a5.rmeta: src\main.rs
+
+c:\Users\user\Desktop\Cmder\dev\docx-template-generation\backend\target\rls\debug\deps\backend-f425bb5e883cf3a5.d: src\main.rs
+
+src\main.rs:
