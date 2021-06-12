10f53c311c8ae1a00fc98413f5627ef1350370ef
Artem <Artemasdwasdw@mail.ru>
2021-06-12 08:44:41 +0300

    implement sending placeholders to api

--- frontend/src/components/pages/TemplatePage/GenerationForm/GenerationForm.tsx
+++ frontend/src/components/pages/TemplatePage/GenerationForm/GenerationForm.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 // local libs
+import { useAppSelector } from '@store'
+import { selectPickedTemplatesData } from '@store/CheckedTemplatesData'
 import { GenerationFormContainer, ButtonContainer } from './styles'
 import { PlaceholdersList } from './PlaceholdersList'
 import { Button } from '@components/generic'
+import { useApi } from '@hooks'
 // types
 import { FormData } from './types'
 
 export const GenerationForm: React.FC = () => {
+  const [preparedFiles, setPreparedFiles] = useState<Array<string>>([])
+  const { getPreparedFileNames } = useApi()
+  const { pickedTemplates } = useAppSelector(selectPickedTemplatesData)
+
   const { register, handleSubmit } = useForm<FormData>()
 
-  const onSubmit = (formData: FormData) => {
-    console.log(formData)
+  const onSubmit = async (formData: FormData) => {
+    const fileNames = await getPreparedFileNames(pickedTemplates, formData)
+    setPreparedFiles(fileNames)
   }
 
   return (
@@ -22,7 +30,9 @@ export const GenerationForm: React.FC = () => {
         <Button type="submit" margin="0 15px 0 0">
           Generate documents
         </Button>
-        <Button>Download documents</Button>
+        <Button disabled={preparedFiles.length === 0}>
+          Download documents
+        </Button>
       </ButtonContainer>
     </GenerationFormContainer>
   )
